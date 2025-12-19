b2cc6c4ecfbd0eca9372333ef85cdab21da762d5
midhun <midhunmadhavps@gmail.com>
2025-12-20 01:18:12 +0530

    slider updated

--- public/shop.php
+++ public/shop.php
@@ -305,7 +305,7 @@
                                                     <div id="slider-range"
                                                         class="ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content">
                                                         <div class="ui-slider-range ui-corner-all ui-widget-header"
-                                                            style="left: 15%; width: 45%;"></div><span tabindex="0"
+                                                            style="left: 0%; width: 0%;"></div><span tabindex="0"
                                                             class="ui-slider-handle ui-corner-all ui-state-default"
                                                             style="left: 15%;"></span><span tabindex="0"
                                                             class="ui-slider-handle ui-corner-all ui-state-default"
